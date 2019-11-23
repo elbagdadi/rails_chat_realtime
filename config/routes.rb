72d0e8eb4ae8b2ebe421ecf21c54d14160a02cf3
Abderrahim EL BAGDADI <elbagdadi.abdou@gmail.com>
2019-11-23 12:56:18 +0100

    sessions views and controller also chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'chatroom#index'
+  get 'sessions/index'
+  get 'sessions/show'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/edit'
+  get 'sessions/update'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
